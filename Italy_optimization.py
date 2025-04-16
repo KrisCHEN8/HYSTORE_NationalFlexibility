b0e6a6f731ded5b369d3d5856b62e61fd003eaff
Yangzhe Chen <yangzhec@UG.KTH.SE>
2025-04-16 17:57:29 +0200

    italy results added

---
+++ Italy_optimization.py
@@ -0,0 +1,104 @@
+import os
+import pandas as pd
+from predictive_optimization import PredictiveOptimizerCVXPY  # noqa: E501
+import warnings
+warnings.filterwarnings("ignore", message="loaded more than 1 DLL from .libs")
+
+solver = 'CVXPY'
+
+coutry = 'IT'      # Change with other coutries' abbreviations, e.g. ESP, SE, AUT  # noqa: E501
+pickle_path = './national_zones/pickled_df'
+df_agg = pd.read_pickle(os.path.join(pickle_path, f'df_agg_{coutry}.pkl'))
+
+df_demand = pd.read_pickle(os.path.join(pickle_path, 'df_demand.pkl'))
+df_emission = pd.read_pickle(os.path.join(pickle_path, f'emission_{coutry}_2022.pkl'))
+
+
+time_series = pd.date_range(start='2022-01-01 00:00:00', periods=8760, freq='1h')  # noqa: E501
+
+# Input data
+D_H = df_demand['Italy_heating_kWh'].values * 0.001    # noqa: E501
+D_C = df_demand['Italy_cooling_kWh'].values * 0.001    # noqa: E501
+
+
+df_demand.index = time_series
+df_emission.index = time_series
+df_agg.index = time_series
+
+COP_df = pd.read_pickle(os.path.join(pickle_path, 'COP_2022_df.pkl'))
+EER_df = pd.read_pickle(os.path.join(pickle_path, 'EER_2022_df.pkl'))
+
+COP_df.index = time_series
+EER_df.index = time_series
+
+hours = 1
+
+heating_season = df_demand[df_demand['Italy_heating_kWh'] > 0]
+avg_heating_demand = heating_season['Italy_heating_kWh'].mean() * 0.001
+mean_COP = COP_df[coutry].mean()
+thermal_capacity_heating = avg_heating_demand * mean_COP
+
+cooling_season = df_demand[df_demand['Italy_cooling_kWh'] > 0]
+avg_cooling_demand = heating_season['Italy_cooling_kWh'].mean() * 0.001
+mean_EER = EER_df[coutry].mean()
+thermal_capacity_cooling = avg_cooling_demand * mean_EER
+
+Cm_dict_ave = {
+    'Cm_h_PCM': thermal_capacity_heating * hours,    # MWh
+    'Cm_c_PCM': thermal_capacity_cooling * hours,    # MWh
+    'Cm_h_TCM': thermal_capacity_heating * hours,    # MWh
+    'Cm_c_TCM': thermal_capacity_cooling * hours     # MWh
+}
+
+Cm_dict_70p = {
+    'Cm_h_PCM': thermal_capacity_heating * hours,   # MWh
+    'Cm_c_PCM': thermal_capacity_cooling * hours,   # MWh
+    'Cm_h_TCM': thermal_capacity_heating * hours,   # MWh
+    'Cm_c_TCM': thermal_capacity_cooling * hours   # MWh
+}
+
+Cm_dict_50p = {
+    'Cm_h_PCM': thermal_capacity_heating * hours,   # MWh
+    'Cm_c_PCM': thermal_capacity_cooling * hours,   # MWh
+    'Cm_h_TCM': thermal_capacity_heating * hours,   # MWh
+    'Cm_c_TCM': thermal_capacity_cooling * hours    # MWh
+}
+
+lambda_value = 1
+
+if solver == 'PSO':
+    print('Pyomo is not available.')
+
+elif solver == 'CVXPY':
+    # Cm_ave
+    optimizer = PredictiveOptimizerCVXPY(D_H, D_C, df_agg, df_emission, 12, COP_df['IT'], EER_df['IT'], Cm_dict_ave, 'surplus_RES')  # noqa: E501
+    df_results = optimizer.opt(time_series[0], time_series[-1], lambda_value=lambda_value)
+    df_results.index = time_series
+    df_results['actual_load'] = df_agg['Actual load'].values
+    heating = - df_results['x_TCM_h'] - df_results['x_PCM_h']  # noqa: E501
+    cooling = - df_results['x_TCM_c'] - df_results['x_PCM_c']  # noqa: E501
+    df_results['modified_load'] = df_results['actual_load'] + heating + cooling
+    df_results['surplus_optimized'] = df_results['surplus'] - (df_results['y_TCM_h'] + df_results['y_PCM_h'] + df_results['y_TCM_c'] + df_results['y_PCM_c'])  # noqa: E501
+    df_results.to_excel(f'./res/IT/results_IT_aveCm_{hours}_lambda{lambda_value}.xlsx', index=True)
+
+    # Cm_70%
+    optimizer = PredictiveOptimizerCVXPY(D_H, D_C, df_agg, df_emission, 12, COP_df['IT'], EER_df['IT'], Cm_dict_70p, 'surplus_RES')  # noqa: E501
+    df_results = optimizer.opt(time_series[0], time_series[-1], lambda_value=lambda_value)
+    df_results.index = time_series
+    df_results['actual_load'] = df_agg['Actual load'].values
+    heating = - df_results['x_TCM_h'] - df_results['x_PCM_h']  # noqa: E501
+    cooling = - df_results['x_TCM_c'] - df_results['x_PCM_c']  # noqa: E501
+    df_results['modified_load'] = df_results['actual_load'] + heating + cooling
+    df_results['surplus_optimized'] = df_results['surplus'] - (df_results['y_TCM_h'] + df_results['y_PCM_h'] + df_results['y_TCM_c'] + df_results['y_PCM_c'])  # noqa: E501
+    df_results.to_excel(f'./res/IT/results_IT_70PCm_{hours}_lambda{lambda_value}.xlsx', index=True)
+
+    # Cm_50%
+    optimizer = PredictiveOptimizerCVXPY(D_H, D_C, df_agg, df_emission, 12, COP_df['IT'], EER_df['IT'], Cm_dict_50p, 'surplus_RES')  # noqa: E501
+    df_results = optimizer.opt(time_series[0], time_series[-1], lambda_value=lambda_value)
+    df_results.index = time_series
+    df_results['actual_load'] = df_agg['Actual load'].values
+    heating = - df_results['x_TCM_h'] - df_results['x_PCM_h']  # noqa: E501
+    cooling = - df_results['x_TCM_c'] - df_results['x_PCM_c']  # noqa: E501
+    df_results['modified_load'] = df_results['actual_load'] + heating + cooling
+    df_results['surplus_optimized'] = df_results['surplus'] - (df_results['y_TCM_h'] + df_results['y_PCM_h'] + df_results['y_TCM_c'] + df_results['y_PCM_c'])  # noqa: E501
+    df_results.to_excel(f'./res/IT/results_IT_50PCm_{hours}_lambda{lambda_value}.xlsx', index=True)
---
+++ national_zones/entsoe_utils.py
@@ -0,0 +1,188 @@
+import os
+import pandas as pd
+import numpy as np
+from entsoe import EntsoePandasClient
+
+
+class EntsoeData:
+    def __init__(self, country, start, end, range_time_generation="1h"):
+        self.client = EntsoePandasClient(api_key='a817ac5b-7d6a-4192-8d24-ff6a990f7d31')
+        self.country = country
+        self.start = start
+        self.end = end
+        self.range_time_generation = range_time_generation
+
+    def get_generation_data(self, zone_name=None, austria=False) -> pd.DataFrame:
+        if zone_name is None:
+            self.country_code = self.country
+        else:
+            self.country_code = self.country + "_" + zone_name
+
+
+        if not austria:
+            df_generation = self.client.query_generation(self.country_code, start=self.start, end=self.end, psr_type=None)
+            date_index = pd.date_range(start=self.start, periods=len(df_generation), freq=self.range_time_generation)
+            df_generation.index = date_index
+            # print(df_generation.columns)
+
+            if isinstance(df_generation.columns, pd.MultiIndex):
+                df_generation.columns = [
+                    (
+                        "Hydro Pumped Storage charge" if col[0] == "Hydro Pumped Storage" and col[1] == "Actual Aggregated"
+                        else "Hydro Pumped Storage discharge" if col[0] == "Hydro Pumped Storage" and col[1] == "Actual Consumption"
+                        else col[0]
+                    )
+                    for col in df_generation.columns
+                ]
+
+            else:
+                pass
+
+            col_count = {}
+            new_columns = []
+
+            for col in df_generation.columns:
+                if col in col_count:
+                    # If the column name has been seen before, append the count as a suffix
+                    new_name = f"{col}_{col_count[col]}"
+                    col_count[col] += 1
+                else:
+                    new_name = col
+                    col_count[col] = 1
+
+                new_columns.append(new_name)
+
+            df_generation.columns = new_columns
+            df_generation.fillna(0, inplace=True)
+
+            print(df_generation.columns)
+
+            print(f'{self.country_code} obtained.')
+
+            return df_generation
+
+        else:
+            df_generation = self.client.query_generation(self.country_code, start=self.start, end=self.end, psr_type=None)
+            date_index = pd.date_range(start=self.start, periods=len(df_generation), freq=self.range_time_generation)
+            df_generation.index = date_index
+            df_generation = df_generation.iloc[:, [0, 2, 4, 6, 17, 8, 12, 18, 20, 22, 24, 10, 14]]
+            # print(df_generation.columns)
+
+            if isinstance(df_generation.columns, pd.MultiIndex):
+                df_generation.columns = [
+                    (
+                        "Hydro Pumped Storage charge" if col[0] == "Hydro Pumped Storage" and col[1] == "Actual Aggregated"
+                        else "Hydro Pumped Storage discharge" if col[0] == "Hydro Pumped Storage" and col[1] == "Actual Consumption"
+                        else col[0]
+                    )
+                    for col in df_generation.columns
+                ]
+
+            else:
+                pass
+
+            col_count = {}
+            new_columns = []
+
+            for col in df_generation.columns:
+                if col in col_count:
+                    # If the column name has been seen before, append the count as a suffix
+                    new_name = f"{col}_{col_count[col]}"
+                    col_count[col] += 1
+                else:
+                    new_name = col
+                    col_count[col] = 1
+
+                new_columns.append(new_name)
+
+            df_generation.columns = new_columns
+            df_generation.fillna(0, inplace=True)
+
+            print(df_generation.columns)
+
+            print(f'{self.country_code} obtained.')
+
+            return df_generation
+
+    def aggregate(self, generation_list: list[pd.DataFrame]) -> pd.DataFrame:
+        try:
+            # Check if the list is empty.
+            if not generation_list:
+                raise ValueError("The dataframe list is empty.")
+
+            # Create a union of all columns from all dataframes.
+            all_columns = set()
+            for df in generation_list:
+                all_columns.update(df.columns)
+            # Optionally, sort the columns for a consistent order.
+            all_columns = sorted(all_columns)
+
+            print(all_columns)
+
+            # Reindex each DataFrame so that it contains all columns, filling missing columns with 0.
+            standardized_list = [
+                df.reindex(columns=all_columns, fill_value=0) for df in generation_list
+            ]
+
+            # Create an aggregated DataFrame by adding the dataframes element-wise.
+            df_aggregated = standardized_list[0].copy()
+            for df in standardized_list[1:]:
+                df_aggregated = df_aggregated.add(df, fill_value=0)
+
+            return df_aggregated
+
+        except Exception as e:
+            raise e
+
+
+def calculate_emission_factor(df: pd.DataFrame) -> pd.DataFrame:
+    other_emission = 870 * 1e3 * 1e-6   # ton CO2/MWh  coal
+    fossilgas_emission = 368.3 * 1e3 * 1e-6  # ton CO2/MWh
+    fossiloil_emission = 548.9 * 1e3 * 1e-6  # ton CO2/MWh
+
+    df_generation = df.copy()
+
+    # If the 'Other' column is present, calculate co2 emissions
+    if 'Other' in df_generation.columns:
+        df_generation['co2_other'] = df_generation['Other'] * other_emission
+
+    # If the 'Fossil Coal-derived gas' column is present, calculate co2 emissions.
+    # Note: In your example, the same `other_emission` is applied.
+    if 'Fossil Coal-derived gas' in df_generation.columns:
+        df_generation['co2_fossilcoal'] = df_generation['Fossil Coal-derived gas'] * fossilgas_emission
+
+    # If the 'Fossil Gas' column is present, calculate co2 emissions.
+    if 'Fossil Gas' in df_generation.columns:
+        df_generation['co2_fossilgas'] = df_generation['Fossil Gas'] * fossilgas_emission
+
+    # If the 'Fossil Oil' column is present, calculate co2 emissions.
+    if 'Fossil Oil' in df_generation.columns:
+        df_generation['co2_fossiloil'] = df_generation['Fossil Oil'] * fossiloil_emission
+
+    if 'Fossil hard coal' in df_generation.columns:
+        df_generation['co2_fossilcoal'] = df_generation['Fossil hard coal'] * other_emission
+
+    # sum the emissions
+    emission_cols = ['co2_other', 'co2_fossilcoal', 'co2_fossilgas', 'co2_fossiloil']
+    existing_cols = [col for col in emission_cols if col in df_generation.columns]
+    df_generation['co2_total'] = df_generation[existing_cols].sum(axis=1)
+
+    if 'Hydro Pumped Storage discharge' in df_generation.columns:
+        # calculate the emission factor
+        df_generation['emission_factor'] = (
+            df_generation['co2_total'] /
+            df.drop(columns=['Hydro Pumped Storage discharge']).sum(axis=1)
+        )
+    else:
+        df_generation['emission_factor'] = df_generation['co2_total'] / df.sum(axis=1)
+
+    return df_generation
+
+
+if __name__ == "__main__":
+    start = pd.Timestamp('20220101', tz='Europe/Rome')
+    end = pd.Timestamp('20230101', tz='Europe/Rome')
+
+    entsoe_data = EntsoeData('AT', start, end, range_time_generation="1h")
+    df_generation = entsoe_data.get_generation_data(austria=True)
+    print(df_generation.columns)
